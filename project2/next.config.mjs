9021865835e32a53df9a15752e6ee6185965e566
Arindam Chowdhury <arindamchy00@gmail.com>
2025-06-17 14:09:02 +0530

    Added dev only for next-webpack configuration

--- project2/next.config.mjs
+++ project2/next.config.mjs
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.watchOptions = {
-      followSymlinks: true,
-    };
-    config.snapshot = {
-      unmanagedPaths: [
-        /^(.+?[\\/]node_modules[\\/](@personal[\\/]project1)(@.+?[\\/])?.+?)[\\/]/,
-      ],
-    };
-
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        followSymlinks: true,
+      };
+      config.snapshot = {
+        unmanagedPaths: [
+          /^(.+?[\\/]node_modules[\\/](@personal[\\/]project1)(@.+?[\\/])?.+?)[\\/]/,
+        ],
+      };
+    }
     return config;
   },
 };
